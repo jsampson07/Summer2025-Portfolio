ed6e260e1d38bf1b57f5935b2d056a3465382485
jsampson35 <jsampson35@gatech.edu>
2025-06-03 16:03:45 -0400

    fixed print statements, minor error in init function, and added boolean flags to removetask and get_task

--- CLI-todo/tasks_db.py
+++ CLI-todo/tasks_db.py
@@ -24,7 +24,7 @@ class TaskDB:
     def __init__ (self, filename="tasks.json"):
         self.filename = filename
         if not os.path.exists(self.filename):
-            new_file = open(self.filename)
+            new_file = open(self.filename, "w")
             json.dump([], new_file)
     def load(self): #used to load information from the json file
         file = open(self.filename)
@@ -38,7 +38,7 @@ class TaskDB:
             #task_info is each dict (task and its other info)
             tname = task_info["Task Name"]
             if tname.lower() == task_name.lower():
-                print("You already have {tname} as a task.")
+                print(f"You already have {tname} as a task.")
                 return
         #Logic to add task to DB
         task_to_add = {"Task Name": task_name, "Description": description, "Due Date": due_date, "Completed": completed, "Priority": priority}
@@ -46,9 +46,10 @@ class TaskDB:
         file = open(self.filename, "w")
         json.dump(file_data, file, indent=2)
         file.close()
-        print("Added {task_name} to your to-do list!")
+        print(f"Added {task_name} to your to-do list!")
     def removetask(self, task_name):
         file_data = self.load()
+        removed = False
         for ind, tasks in enumerate(file_data):
             tname = tasks["Task Name"]
             if tname.lower() == task_name.lower():
@@ -58,17 +59,23 @@ class TaskDB:
                 file = open(self.filename, "w")
                 json.dump(file_data, file, indent=2)
                 file.close()
-                print("Successfully removed your {task_name} task!")
-        print("{task_name} does not exist. Could not remove.")
+                removed = True
+                print(f"Successfully removed your {task_name} task!")
+                break
+        if not removed:
+            print(f"{task_name} does not exist. Could not remove.")
     def get_all_tasks(self):
         all_tasks = self.load()
-        print("Here are all your tasks:\n{all_tasks}")
+        print(f"Here are all your tasks:\n{all_tasks}")
     def get_task(self, task_name):
         file_data = self.load() #we now have a list (type = list)
         #Logic to check if task already exists
+        found_task = False
         for task_info in file_data:
             #task_info is each dict (task and its other info)
             tname = task_info["Task Name"]
             if tname.lower() == task_name.lower():
-                print("Here is your task and its correspinding information:\n{task_info}")
-        print("You do not have a {task_name} task.")
+                found_task = True
+                print(f"Here is your task and its correspinding information:\n{task_info}")
+        if not found_task:
+            print(f"You do not have a {task_name} task.")
