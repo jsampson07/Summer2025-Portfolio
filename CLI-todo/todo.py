2c7a0b7f06f38ed8f39741ed19a190e5dda39d74
jsampson35 <jsampson35@gatech.edu>
2025-06-03 18:44:50 -0400

    added datetime checks for due_date field for tasks and added 'remall' command and functionality

--- CLI-todo/todo.py
+++ CLI-todo/todo.py
@@ -2,6 +2,13 @@
 import json
 from tasks_db import TaskDB
 import argparse
+from datetime import datetime
+
+def validate_date(date):
+    try:
+        return datetime.strptime(date, "%m-%d-%Y")
+    except ValueError:
+        raise argparse.ArgumentTypeError("Due-Date must be in the format MM-DD-YYYY")
 
 def main():
     db = TaskDB() #create TaskDB instance to use throughout the code
@@ -18,7 +25,7 @@ def main():
     add_parser  = subparsers.add_parser("add", help="Add a new task")
     add_parser.add_argument("task", type=str, help="Name of task to add")
     add_parser.add_argument("--description", type=str, help="Add a description for the task (optional)")
-    add_parser.add_argument("--due_date", type=str, help="Put in a due date (optional)")
+    add_parser.add_argument("--due_date", type=validate_date, help="Put in a due date (optional)")
     add_parser.add_argument("--completed", action="store_true", help="Completed or not? (True or False)")
         #action keyword argument ==> "store_true" stores True if flag is present
     add_parser.add_argument("priority", type=str, help="Put 'low', 'medium', or 'high' priority for your task")
@@ -32,13 +39,15 @@ def main():
     #remove subparser
     remove_parser = subparsers.add_parser("remove", help="Remove a specified task")
     remove_parser.add_argument("task", type=str, help="Name of the task to remove")
+    
+    remove_all_parser = subparsers.add_parser("remall", help="Remove all tasks") #mainly used for testing
 
     #update subparser
     update_parser = subparsers.add_parser("update", help="Update an existing task")
     update_parser.add_argument("task", type=str, help="Name of task to update")
     update_parser.add_argument("--taskname", type=str, help="new name")
     update_parser.add_argument("--description", type=str, help="Add a description for the task (optional)")
-    update_parser.add_argument("--due_date", type=str, help="Put in a due date (optional)")
+    update_parser.add_argument("--due_date", type=validate_date, help="Put in a due date (optional)")
     update_parser.add_argument("--completed", action="store_true", help="Completed or not? (True or False)")
         #action keyword argument ==> "store_true" stores True if flag is present
     update_parser.add_argument("--priority", type=str, help="Put 'low', 'medium', or 'high' priority for your task")
@@ -58,6 +67,8 @@ def main():
         db.removetask(args.task)
     elif args.command == "update":
         db.update_task(args.task, args.taskname, args.description, args.due_date, args.completed, args.priority)
+    elif args.command == "remall":
+        db.remove_all()
 
 if __name__ == "__main__":
     main()
