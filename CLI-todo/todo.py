b65734ca8a3cff7e3bc8ea3b667ac66c001b7941
jsampson35 <jsampson35@gatech.edu>
2025-06-03 16:06:55 -0400

    added parser logic for add, list, and remove commands and their optional arguments

--- CLI-todo/todo.py
+++ CLI-todo/todo.py
@@ -12,27 +12,40 @@ def main():
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     #access "add", "list", "remove", etc inputs via COMMAND var
+    #for any arguments not specified (if optional) value = None (blank)
 
     #add subparser
     add_parser  = subparsers.add_parser("add", help="Add a new task")
     add_parser.add_argument("task", type=str, help="Name of task to add")
     add_parser.add_argument("--description", type=str, help="Add a description for the task (optional)")
     add_parser.add_argument("--due_date", type=str, help="Put in a due date (optional)")
-    add_parser.add_argument("--completed", type=bool, help="Completed or not? (True or False)")
+    add_parser.add_argument("--completed", action="store_true", help="Completed or not? (True or False)")
+        #action keyword argument ==> "store_true" stores True if flag is present
     add_parser.add_argument("priority", type=str, help="Put 'low', 'medium', or 'high' priority for your task")
 
     #list subparser
     #for list parsers we want to be able to list either all tasks or one task
     list_parser = subparsers.add_parser("list", help="List all your tasks")
         #below allows us to specify ALL (optional) OR a specific task
-    list_parser.add_argument("task", type=str, help="Name of the task to list")
+    list_parser.add_argument("--task", type=str, help="Name of the task to list")
 
     #remove subparser
     remove_parser = subparsers.add_parser("remove", help="Remove a specified task")
-    remove_parser.add_argument("remove_task", type=str, help="Name of the task to remove")
-
+    remove_parser.add_argument("task", type=str, help="Name of the task to remove")
 
     args = parser.parse_args()
+    #print(type(args))
+    #args creates a Namespace object that has all attributes assigned to it for current iteration
+        #i.e. args.command, args.priority, args.task (does not matter abt subparsers --> the overall argparse.Namspace obj has all attributes)
+    if args.command == "add":
+        db.addtask(args.task, args.description, args.due_date, args.completed, args.priority)
+    elif args.command == "list":
+        if args.task:
+            db.get_task(args.task)
+        else:
+            db.get_all_tasks()
+    elif args.command == "remove":
+        db.removetask(args.task)
 
 if __name__ == "__main__":
     main()
