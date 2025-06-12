cf71c6b6f3bb435a22b82a5a66cc930403d496ca
jsampson35 <jsampson35@gatech.edu>
2025-06-12 00:20:03 -0400

    Final version of banner, realized cannot test using local machine

--- week2/banner_grabber.py
+++ week2/banner_grabber.py
@@ -1,24 +1,24 @@
-from scapy.all import IP, TCP, Raw, sr, sr1, sniff, send, AsyncSniffer
+from scapy.all import IP, TCP, Raw, sr, sr1, sniff, send, AsyncSniffer, RandShort
 import pprint
 import netifaces
 
 def banner_grabber():
-    my_network = "127.0.0.1"
-    for port in [22, 8000, 443]:
-        syn_pckt = IP(dst=my_network) / TCP(dport=port, flags="S")
+    my_network = "scanme.nmap.org"
+    for port in [22, 80, 443]:
+        my_sport = RandShort()
+        syn_pckt = IP(dst=my_network) / TCP(sport=my_sport, dport=port, flags="S")
         print(syn_pckt[TCP].sport)
-        sa_resp = sr1(syn_pckt, timeout=2)
-        if not sa_resp or sa_resp[TCP].flags & 0x12 != 0x12:
+        syn_ack = sr1(syn_pckt, timeout=2)
+        if not syn_ack or syn_ack[TCP].flags & 0x12 != 0x12:
             print(f"Port {port} closed or filtered; skipping")
             continue
-        seq = sa_resp[TCP].ack
-        ack = sa_resp[TCP].seq + 1
-        client_port = sa_resp[TCP].dport # we need this bc Scapy will picks a new random source port for each request if not specified
+        sequence = syn_ack[TCP].ack
+        ackn = syn_ack[TCP].seq + 1
+        client_port = syn_ack[TCP].dport # we need this bc Scapy will picks a new random source port for each request if not specified
         print(client_port)
-        a_pckt = IP(dst=my_network) / TCP(sport=client_port, dport=port,  seq=seq, ack=ack)
+        a_pckt = IP(dst=my_network) / TCP(sport=my_sport, dport=port, flags="A", seq=sequence, ack=ackn)
         
-        banner_req = sr1(a_pckt, timeout=2)
-        banner_req.show()
+        banner_req = sr1(a_pckt, timeout=5)
         if banner_req:
             print(f"Port {port}, Banner: {banner_req}")
         else:
