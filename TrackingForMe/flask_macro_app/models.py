eff41289f4d2e62bd964f8dfd89b27215e5620a7
jsampson35 <jsampson35@gatech.edu>
2025-07-08 01:30:31 -0400

    Minor changes to model to fix table referencing, migration script added

--- TrackingForMe/flask_macro_app/models.py
+++ TrackingForMe/flask_macro_app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 from flask_macro_app import db
 import sqlalchemy as sa  # Used for fundamental building blocks for the database schema i.e. defining data types
@@ -16,6 +17,10 @@ class ServingUnit(enum.Enum):
     ML = "ml"
     CUP = "cup"
     UNIT = "unit"
+
+meal_items = db.Table("meal_items", db.Column("meal_id", db.Integer, db.ForeignKey("Meals.id"), primary_key=True),
+                    db.Column("food_id", db.Integer, db.ForeignKey("Foods.id"), primary_key=True),
+                    db.Column("quantity", db.Numeric(10,2)))
 
 class User(db.Model):
     # so.Mapped[int]/so.Mapped[float] - defines the type of the column (required)
@@ -47,6 +52,8 @@ class Food(db.Model):
 
     user_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("Users.id"), nullable=True)  # If food is in pre-existing catalog, no user id, else: userid of creator
 
+    meals: so.Mapped[list["Meal"]] = so.relationship(secondary=meal_items, back_populates="foods")
+
     def __repr__(self):
         return f"<Food {self.name}>"
     
@@ -54,9 +61,23 @@ class Meal(db.Model):
     __tablename__ = "Meals"
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(128), unique=True, index=True, nullable=False)
-    saved: so.Mapped[bool] = so.mapped_column(nullable=True)
+    saved: so.Mapped[bool] = so.mapped_column(nullable=True, default=False)
 
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Users.id"), nullable=False)  # Meal must be created by a user
 
+    foods: so.Mapped[list["Food"]] = so.relationship(secondary=meal_items, back_populates="meals")
+
     def __repr__(self):
         return f"<Meal {self.name}>"
+    
+class DailyLog(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    log_date: so.Mapped[datetime] = so.mapped_column(sa.DateTime(), nullable=False, index=True)
+    quantity_consumed: so.Mapped[float] = so.mapped_column(sa.Float, nullable=False, default=1)  # default store as 1 if "null"
+
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("Users.id"), nullable=False)  # Log entry must always correspond to a user
+    food_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("Foods.id"), nullable=True)
+    meal_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("Meals.id"), nullable=True)
+
+    def __repr__(self):
+        return f"<DailyLog {self.log_date}>"
